aaf006019e6c248e0398359b7deb672cf3253b66
Mariano Martinotti <mariano.andres1024@gmail.com>
2025-06-04 23:51:54 -0300

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
+import {VitePWA} from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +10,51 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType:'autoUpdate',
+      includeAssets:['logo-basico-tensiora.ico','robots.txt'],
+      manifest:{
+        name:"Tensiora Electric",
+        short_name:"Tensiora",
+        description:"Aplicacion para electricista",
+        theme_color:'#222831',
+        background_color:'#F7F7F7',
+        display:'standalone',
+        start_url:'/tensioraElectric/',
+        icons:[
+           {
+             src:'/192x192.png',
+             sizes:'192x192',
+             type:'image/png',
+           },
+           {
+            src:'/512x512.png',
+            sizes:'512x512',
+            type:'image/png',
+           },
+        ],
+      
+      },
+      workbox:{
+        runtimeCaching:[
+           {
+              urlPattern:/^https:\/\/mi-api\.com\/.*$/,
+              handler:'NetworkFirst',
+              options:{
+                cacheName:'mi-api-cache',
+                expiration:{
+                  maxEntries:50,
+                  maxAgeSeconds:60*60*24,
+                },
+                cacheableResponse:{
+                  statuses:[0,200],
+                }
+              }
+           }
+        ]
+      }
+
+    })
   ],
  
 })
