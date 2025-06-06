565db1a15535de95db964e6bffeccfb11a495d68
Mariano Martinotti <mariano.andres1024@gmail.com>
2025-06-06 12:49:40 -0300

    add files

--- src/components/VideoEmbebido/VideoEmbebido.tsx
+++ src/components/VideoEmbebido/VideoEmbebido.tsx
@@ -9,17 +9,20 @@ export const VideoEmbebido: React.FC = () => {
                 width="560"
                 height="315"
                 src="https://www.youtube.com/embed/kHZ8SD7jiiA?si=2MAAq5cZHQ0Knju-"
-                title="YouTube video player"
+                title="Circuitos electricos del traductor de ingenieria"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
+                className='py-2 '
                 style={{
 
                     width: '100%',
 
+
                 }}
             />
+            <span className='mx-2'><b>Provided By:</b> <a href="https://www.youtube.com/@eltraductor_ok" target='_blank'>El traductor de Ingenieria</a></span>
 
 
         </section>
