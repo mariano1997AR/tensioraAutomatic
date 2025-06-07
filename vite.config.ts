331cb3aea6f743aaa0109330bd01e1474d798a09
Mariano Martinotti <mariano.andres1024@gmail.com>
2025-06-07 17:13:55 -0300

    added 1.2.7

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 import {VitePWA} from 'vite-plugin-pwa';
+import {visualizer} from 'rollup-plugin-visualizer';
 
 
 // https://vite.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig({
   base:"/tensioraElectric/",
   plugins: [
     react(),
+    visualizer(),
     tailwindcss(),
     VitePWA({
       registerType:'autoUpdate',
