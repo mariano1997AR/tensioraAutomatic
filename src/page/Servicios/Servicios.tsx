583664057c17e54862701fafe4a264220751c9cc
Mariano Martinotti <mariano.andres1024@gmail.com>
2025-06-22 17:20:21 -0300

    add && changed

--- src/page/Servicios/Servicios.tsx
+++ src/page/Servicios/Servicios.tsx
@@ -14,46 +14,46 @@ export const Servicios: React.FC = () => {
 
                     <article className='item-servicios' style={{ borderColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }} >
                         <h2 className='text-center py-3 subtitulo ' style={{ borderBottomColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }}>Desarrollo web</h2>
-                        <h3 className='mx-4'>Servicios</h3>
+                    
                         <ul>
-                            <li><b>Landing pages</b> optimizadas para SEO</li>
-                            <li><b>Sitios web institucionales</b> para negocios </li>
-                            <li><b>Tiendas online (e-commerce)</b> con codigo personalizado</li>
-                            <li><b>Desarrollo full-stack</b> (frontend,backend y base de datos)</li>
-                            <li><b>Integraciones con APIs</b> (Google Maps, Stripe, redes sociales, etc)</li>
-                            <li><b>Sistenas de gestión(ERP,CRM)</b> personalizados</li>
+                            <li><b> - Landing pages</b> optimizadas para SEO</li>
+                            <li><b> - Sitios web institucionales</b> para negocios </li>
+                            <li><b> - Tiendas online (e-commerce)</b> con codigo personalizado</li>
+                            <li><b> - Desarrollo full-stack</b> (frontend,backend y base de datos)</li>
+                            <li><b> - Integraciones con APIs</b> (Google Maps, Stripe, redes sociales, etc)</li>
+                            <li><b> - Sistenas de gestión(ERP,CRM)</b> personalizados</li>
                         </ul>
 
 
                     </article>
                     <article className='item-servicios' style={{ borderColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }}>
                          <h2 className='text-center py-3 subtitulo' style={{ borderBottomColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }}>Automatización y Scripts</h2>
-                         <h3 className='mx-4'>Servicios</h3>
+                
                          <ul>
-                            <li>Automatización de tareas repetitivas (descargas,reportes, backups)</li>
-                            <li>Bots para redes sociales,Telegram, Whatsapp</li>
-                            <li>Raspado de datos (Web Scrapping)</li>
-                            <li>Conectores entre plataformas (ej: Google Sheets, Gmail)</li>
+                            <li> - Automatización de tareas repetitivas (descargas,reportes, backups)</li>
+                            <li> - Bots para redes sociales,Telegram, Whatsapp</li>
+                            <li> - Raspado de datos (Web Scrapping)</li>
+                            <li> - Conectores entre plataformas (ej: Google Sheets, Gmail)</li>
                          </ul>
                     </article>
                     <article className='item-servicios' style={{ borderColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }}>
                          <h2 className='text-center py-3 subtitulo' style={{ borderBottomColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }} >Base de datos</h2>
-                         <h3 className='mx-4'>Servicios</h3>
+                      
                          <ul>
-                             <li>Diseño de base de datos</li>
-                             <li>Migración y optimización</li>
-                             <li>Creación de paneles con dashboards</li>
-                             <li>Backups y restauración de datos</li>
+                             <li> - Diseño de base de datos</li>
+                             <li> - Migración y optimización</li>
+                             <li> - Creación de paneles con dashboards</li>
+                             <li> - Backups y restauración de datos</li>
                          </ul>
                     </article>
                     <article className='item-servicios' style={{ borderColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }}>
                          <h2 className='text-center py-3 subtitulo' style={{ borderBottomColor: theme === "dark" ? ' #F5F5F5' : '#4A4947' }} >AI y Machine Learning</h2>
-                         <h3 className='mx-4'>Servicios</h3>
+                        
                          <ul>
-                              <li>Chatbots personalizados</li>
-                              <li>Análisis predictivos</li>
-                              <li>Visión artificial (reconocimiento de imágenes)</li>
-                              <li>Clasificación de datos (clientes, productos,etc)</li>
+                              <li> - Chatbots personalizados</li>
+                              <li> - Análisis predictivos</li>
+                              <li> - Visión artificial (reconocimiento de imágenes)</li>
+                              <li> - Clasificación de datos (clientes, productos,etc)</li>
 
                          </ul>
                     </article>
