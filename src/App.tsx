b4f0f6fc66ecb1979a9f55efaa711626a03c078b
Mariano Martinotti <mariano.andres1024@gmail.com>
2025-06-23 12:54:21 -0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-//import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-//import { Loader } from './components/Loader/Loader';
-import { Routes,Route } from 'react-router-dom';
+import { Loader } from './components/Loader/Loader';
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { Home } from './page/Home/Home';
 import { Voltajes } from './page/Voltajes/Voltajes';
@@ -21,55 +21,55 @@ import { TipoCables } from './page/TipoCables/TipoCables';
 import { PublicRoute } from './components/PublicRoute/PublicRoute';
 import { Servicios } from './page/Servicios/Servicios';
 
-export const App:React.FC =()=> {
-//  const [loading,setLoading] = useState<boolean>(true);
- /*
-  useEffect(()=>{
-    setTimeout(()=>{
-        setLoading(false);
-    },3500)
-  },[]);*/
+export const App: React.FC = () => {
+   const [loading, setLoading] = useState<boolean>(true);
 
-  return (
-    <>
-     
-          <Routes>
-               <Route element={<Layout />} >
-                  <Route path='/' element={<Home />}></Route>
-                  <Route path='/home' element={<Home />} />
-                  <Route path='/calcularVoltajes' element={<Voltajes />} />
-                  <Route path='/circuito-en-paralelo' element={<CircuitoParalelo />} />
-                  <Route path='/circuito-en-serie' element={<CircuitoSerie />} />
-                  <Route path='/potencia-tension' element={<Potencia />} />
-                  <Route path='/corriente-monofasico' element={<CorrienteMonofasico />} />
-                  <Route path='/corriente-trifasico' element={<CorrienteTrafasico />} />
-                  <Route path='/herramientas/formulas-para-electricistas' element={<Formulas />} />
-                  <Route path='/contacto' element={<Contacto />} />
-                  <Route path='/circuito-mixto' element={<CircuitoMixto />} />
-                  <Route path='/terminos-condiciones' element={<TerminosCondiciones />} />
-                  <Route path='/servicios' element={<Servicios />} />
-                  <Route path='/ingresar' element={
-                      <PublicRoute>
-                           <Ingresar />
-                      </PublicRoute>
-                  } />
-                  <Route path='/herramientas/tipo-de-cables' element={<TipoCables />}/>
-               </Route>
-               <Route 
-                  path='/dashboard'
-                  
-                  element={
-                     <PrivateRoute >
-                          <Dashboard  />
-                     </PrivateRoute>
-                  }
-                  
-               />
-               
-          </Routes>
+   useEffect(() => {
+      setTimeout(() => {
+         setLoading(false);
+      }, 3500)
+   }, []);
 
-       
-      
-    </>
-  )
+   return (
+      <>
+
+         {loading ? <Loader /> : <Routes>
+            <Route element={<Layout />} >
+               <Route path='/' element={<Home />}></Route>
+               <Route path='/home' element={<Home />} />
+               <Route path='/calcularVoltajes' element={<Voltajes />} />
+               <Route path='/circuito-en-paralelo' element={<CircuitoParalelo />} />
+               <Route path='/circuito-en-serie' element={<CircuitoSerie />} />
+               <Route path='/potencia-tension' element={<Potencia />} />
+               <Route path='/corriente-monofasico' element={<CorrienteMonofasico />} />
+               <Route path='/corriente-trifasico' element={<CorrienteTrafasico />} />
+               <Route path='/herramientas/formulas-para-electricistas' element={<Formulas />} />
+               <Route path='/contacto' element={<Contacto />} />
+               <Route path='/circuito-mixto' element={<CircuitoMixto />} />
+               <Route path='/terminos-condiciones' element={<TerminosCondiciones />} />
+               <Route path='/servicios' element={<Servicios />} />
+               <Route path='/ingresar' element={
+                  <PublicRoute>
+                     <Ingresar />
+                  </PublicRoute>
+               } />
+               <Route path='/herramientas/tipo-de-cables' element={<TipoCables />} />
+            </Route>
+            <Route
+               path='/dashboard'
+
+               element={
+                  <PrivateRoute >
+                     <Dashboard />
+                  </PrivateRoute>
+               }
+
+            />
+
+         </Routes>
+         }
+
+
+      </>
+   )
 }
